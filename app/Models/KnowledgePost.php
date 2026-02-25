06ac917a94f0d5097988f051bd4f9b0a16560217
nidasi <otudaa@gmail.com>
2026-02-25 16:56:12 +0900

    HasFactoryのスペルミス修正

--- app/Models/KnowledgePost.php
+++ app/Models/KnowledgePost.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class KnowledgePost extends Model
 {
-    //
+    use HasFactory;
+
+    //Resident(入居者)との関係:多対1
+    public function resident()
+    {
+        return $this->belongsTo(Resident::class);
+    }
+
+    //Tag(タグ)との関係:多対多
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
