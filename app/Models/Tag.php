20ae236ccea7e5b77b94db9084ec1400988ab8b5
nidasi <otudaa@gmail.com>
2026-03-03 16:39:52 +0900

    Tag.phpにbelongsToMany追加

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Tag extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'tag_name',
+    ];
+
+    //KnowledgePost と多対多
+    public function knowledgePosts()
+    {
+        return $this->belongsToMany(KnowledgePost::class, 'knowledge_post_tag');
+    }
 }
